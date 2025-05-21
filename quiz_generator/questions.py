2860c9092ad4a54f0d03e78a3838fe6f71860145
ELarga-2206 <earl.larga22@gmail.com>
2025-05-21 21:44:50 +0800

    re add quiz_generator as a normal folder

---
+++ quiz_generator/questions.py
@@ -0,0 +1,8 @@
+# quiz_maker files
+
+# main.py             # for the main prog
+# question.py         # how the prog will handle questions
+# quiz_manager.py     # file operation
+# user_interface.py   # display questions, input and output
+
+#blueprint for oop convertion
