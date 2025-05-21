6d23164af340fe1ac43100909f0e59d4d46c4948
ELarga-2206 <earl.larga22@gmail.com>
2025-05-21 23:51:06 +0800

    class user_input -- for handling input and output from the user

---
+++ quiz_generator/user_input.py
@@ -0,0 +1,10 @@
+# for class user_input
+
+class UserInterface:
+    @staticmethod
+    def get_question_text():
+        return input("Enter your question (type 'a' to stop): ")
+    
+    # for the choices 
+
+    def get_choices():
