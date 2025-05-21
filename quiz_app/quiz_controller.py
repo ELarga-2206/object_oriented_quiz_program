eca2910f867d53e8ad10f5d1b85a3b7925fb0e6c
ELarga-2206 <earl.larga22@gmail.com>
2025-05-22 02:20:08 +0800

    Imported the necessary libraries and files – also initialized the setup for the quiz application, user interface, timer etc

--- quiz_app/quiz_controller.py
+++ quiz_app/quiz_controller.py
@@ -1,3 +1,17 @@
 # for quiz_controller
 
-# mainly for the main application of logic
+import time
+import winsound
+from quiz_model import QuizModel
+from quiz_view import QuizView
+from constants import *
+
+class QuizController:
+    def __init__(self, root):
+        self.model = QuizModel()
+        self.view = QuizView(root)
+        self.start_time = time.time()
+        
+        self.setup_handlers()
+        self.update_timer()
+        self.display_current_question()
