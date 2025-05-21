29eb987a65823b9e4faf85c61bf1b4ff5781c774
ELarga-2206 <earl.larga22@gmail.com>
2025-05-22 00:36:24 +0800

    Created and completed the QuizMaker class – providing a user interface for creating and saving quiz questions. sets up the main application loop, allowing users to: - Input question text, choices, and correct answers via the UserInterface

--- quiz_generator/main.py
+++ quiz_generator/main.py
@@ -1,4 +1,4 @@
-from question import Question
+from questions import Question
 from quiz_manager import QuizManager
 from user_input import UserInterface
 
@@ -6,3 +6,31 @@ class QuizMaker:
     def __init__(self):
         self.ui = UserInterface()
         self.manager = QuizManager()
+
+def run(self):
+        while True:
+            question_text = self.ui.get_question_text()
+            
+            if question_text.lower() == 'a':
+                self.ui.show_message("\nQuiz creation complete. Questions saved to 'quiz_questions.txt'")
+                break
+            
+            try:
+                choices = self.ui.get_choices()   # for question details
+                correct_answer = self.ui.get_correct_answer()
+                
+                # save question
+                question = Question(question_text, choices, correct_answer)
+                self.manager.save_question(question)
+                self.ui.show_message("Question saved successfully!")
+                
+                if not self.ui.ask_to_continue():  # check if want to continue
+                    self.ui.show_message("\nQuiz creation complete. Questions saved to 'quiz_questions.txt'")
+                    break
+            
+            except Exception as e:
+                self.ui.show_message(f"\nAn error occurred: {e}\nPlease try again.")
+
+if __name__ == "__main__":
+    app = QuizMaker()
+    app.run()
